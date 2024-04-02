4dac29867945244fb8459500fbc954a063b09cf0
Mostafa Elgreatly <66712535+mostafaroshdy1@users.noreply.github.com>
2024-04-02 21:23:19 +0200

    feat: add matching with other players randomly

---
+++ app.js
@@ -0,0 +1,96 @@
+import express from 'express';
+import { Server } from 'socket.io'
+import { createServer } from 'node:http';
+import ejs from 'ejs';
+
+
+const app = express()
+const server = createServer(app);
+const io = new Server(server);
+const PORT = process.env.PORT || 3000;
+
+app.engine('html', ejs.renderFile);
+app.set('view engine', ejs);
+app.use(express.static('public'));
+
+
+
+const clients = {};
+
+app.get("/", (req, res) => {
+    res.render('index.html')
+})
+
+const players = {};
+let unmatched;
+
+io.on("connection", function (socket) {
+    let id = socket.id;
+
+    console.log("New client connected. ID: ", socket.id);
+    clients[socket.id] = socket;
+
+    socket.on("disconnect", () => {
+        console.log("Client disconnected. ID: ", socket.id);
+        delete clients[socket.id];
+        socket.broadcast.emit("clientdisconnect", id);
+    });
+
+    join(socket);
+
+    if (opponentOf(socket)) {
+        socket.emit("game.begin", {
+            symbol: players[socket.id].symbol
+        });
+
+        opponentOf(socket).emit("game.begin", {
+            symbol: players[opponentOf(socket).id].symbol
+        });
+    }
+
+
+    socket.on("make.move", function (data) {
+        if (!opponentOf(socket)) {
+            return;
+        }
+
+        socket.emit("move.made", data);
+        opponentOf(socket).emit("move.made", data);
+    });
+
+    socket.on("disconnect", function () {
+        if (opponentOf(socket)) {
+            opponentOf(socket).emit("opponent.left");
+        }
+    });
+});
+
+
+
+function join(socket) {
+    players[socket.id] = {
+        opponent: unmatched,
+        symbol: "X",
+        socket: socket
+    };
+
+
+    if (unmatched) {
+        players[socket.id].symbol = "O";
+        players[unmatched].opponent = socket.id;
+        unmatched = null;
+    } else {
+        unmatched = socket.id;
+    }
+}
+
+function opponentOf(socket) {
+    if (!players[socket.id].opponent) {
+        return;
+    }
+    return players[players[socket.id].opponent].socket;
+}
+
+server.listen(PORT, () => {
+    console.log(`server running at http://localhost:${PORT}`);
+})
---
+++ public/scripts/main.js
@@ -0,0 +1,101 @@
+const url = window.location.origin;
+let socket = io.connect(url);
+
+let myTurn = true;
+let symbol;
+
+function getBoardState() {
+    const obj = {};
+
+    document.querySelectorAll(".board button").forEach(button => {
+        obj[button.id] = button.textContent || "";
+    });
+
+    return obj;
+}
+
+function isGameOver() {
+    const state = getBoardState();
+    const matches = ["XXX", "OOO"];
+    const rows = [
+        state.r0c0 + state.r0c1 + state.r0c2,
+        state.r1c0 + state.r1c1 + state.r1c2,
+        state.r2c0 + state.r2c1 + state.r2c2,
+        state.r0c0 + state.r1c0 + state.r2c0,
+        state.r0c1 + state.r1c1 + state.r2c1,
+        state.r0c2 + state.r1c2 + state.r2c2,
+        state.r0c0 + state.r1c1 + state.r2c2,
+        state.r0c2 + state.r1c1 + state.r2c0
+    ];
+
+    for (let i = 0; i < rows.length; i++) {
+        if (rows[i] === matches[0] || rows[i] === matches[1]) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function renderTurnMessage() {
+    const messageElement = document.getElementById("message");
+
+    if (!myTurn) {
+        messageElement.textContent = "Your opponent's turn";
+        document.querySelectorAll(".board button").forEach(button => button.disabled = true);
+    } else {
+        messageElement.textContent = "Your turn.";
+        document.querySelectorAll(".board button").forEach(button => button.disabled = false);
+    }
+}
+
+function makeMove(e) {
+    if (!myTurn) {
+        return;
+    }
+
+    if (e.target.textContent) {
+        return;
+    }
+
+    socket.emit("make.move", {
+        symbol: symbol,
+        position: e.target.id
+    });
+}
+
+socket.on("move.made", function (data) {
+    document.getElementById(data.position).textContent = data.symbol;
+
+    myTurn = data.symbol !== symbol;
+
+    if (!isGameOver()) {
+        renderTurnMessage();
+    } else {
+        const messageElement = document.getElementById("message");
+        if (myTurn) {
+            messageElement.textContent = "You lost.";
+        } else {
+            messageElement.textContent = "You won!";
+        }
+
+        document.querySelectorAll(".board button").forEach(button => button.disabled = true);
+    }
+});
+
+socket.on("game.begin", function (data) {
+    symbol = data.symbol;
+    myTurn = symbol === "X";
+    renderTurnMessage();
+});
+
+socket.on("opponent.left", function () {
+    const messageElement = document.getElementById("message");
+    messageElement.textContent = "Your opponent left the game.";
+    document.querySelectorAll(".board button").forEach(button => button.disabled = true);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".board button").forEach(button => button.disabled = true);
+    document.querySelectorAll(".board button").forEach(button => button.addEventListener("click", makeMove));
+});
